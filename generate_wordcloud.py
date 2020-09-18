84648b9ecaadea2ac22c95dabafadac10f199587
Nguyen The Tuyen <nttuyenx@gmail.com>
2020-09-18 10:27:38 +0700

    Add the file path command-line argument

--- generate_wordcloud.py
+++ generate_wordcloud.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import wordcloud
 import string
 from matplotlib import pyplot as plt
@@ -41,23 +42,29 @@ def main():
     Generate a 'word cloud' from a input text.
     """
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file_path', type=str, help="Path to the text file")
+    args = parser.parse_args()
+    file_path = args.file_path
+
     lines = []
-    with open("./romeo_and_juliet.txt", "r") as fhand:
+    with open(file_path, "r") as fhand:
         for line in fhand:
             lines.append(line)
     file_contents = " ".join(lines)
     words_dict = calculate_frequencies(file_contents)
 
-    # Generate word cloud
+    # Generate a word cloud and save it to a JPEG file
     cloud = wordcloud.WordCloud()
     cloud.generate_from_frequencies(words_dict)
     myimage = cloud.to_array()
-
     plt.imshow(myimage, interpolation = 'nearest')
     plt.axis('off')
     fig = plt.gcf()
     plt.show()
-    fig.savefig('romeo_and_juliet_wordcloud.jpeg', dpi=100, pil_kwargs={'quality':95})
+    file_format = "jpeg"
+    wordcloud_file_name = "{}_wordcloud.{}".format(file_path.split('/')[-1][:-4], file_format)
+    fig.savefig(wordcloud_file_name, dpi=100, pil_kwargs={'quality':95})
 
 
 if __name__ == "__main__":
